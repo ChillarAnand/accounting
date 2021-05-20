90a0fee4d818740c0771ac799be5d0edacf76bad
ChillarAnand <anand21nanda@gmail.com>
2021-05-20 14:18:28 +0530

    feat: Added validations for journal entry

--- accounting/accounting/doctype/journal_entry/journal_entry.js
+++ accounting/accounting/doctype/journal_entry/journal_entry.js
@@ -1,8 +1,34 @@
 // Copyright (c) 2021, ac and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Journal Entry', {
-	// refresh: function(frm) {
+function calculate_total(frm, name) {
+    var entries = frm.doc.accounting_entries;
+    var total = 0;
+    for(var i in entries) {
+        total = total + entries[i][name];
+    }
+    return total;
+}
 
-	// }
+frappe.ui.form.on('Journal Entry Item', 'debit', function(frm, cdt, cdn) {
+    var total = calculate_total(frm, 'debit');
+    frm.set_value('total_debit', total);
 });
+
+
+frappe.ui.form.on('Journal Entry Item', 'credit', function(frm, cdt, cdn) {
+    var total = calculate_total(frm, 'credit');
+    frm.set_value('total_credit', total);
+})
+
+
+frappe.ui.form.on('Journal Entry', 'total_debit', function(frm) {
+    var difference = frm.doc.total_debit - frm.doc.total_credit;
+    frm.set_value('difference', difference);
+})
+
+
+frappe.ui.form.on('Journal Entry', 'total_credit', function(frm) {
+    var difference = frm.doc.total_debit - frm.doc.total_credit;
+    frm.set_value('difference', difference);
+})
--- accounting/accounting/doctype/journal_entry/journal_entry.py
+++ accounting/accounting/doctype/journal_entry/journal_entry.py
@@ -1,8 +1,11 @@
 # Copyright (c) 2021, ac and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
+
 class JournalEntry(Document):
-	pass
+	def validate(self):
+		if self.difference != 0:
+			frappe.throw('Total debit is not equal to total credit.')
