9726e6af3f595437e31519260e167138c7a5a3a9
ChillarAnand <anand21nanda@gmail.com>
2021-05-24 08:44:33 +0530

    feat: Added revere gl entries

--- accounting/accounting/doctype/gl_entry/utils.py
+++ accounting/accounting/doctype/gl_entry/utils.py
@@ -1,4 +1,5 @@
 import frappe
+from frappe.utils import now
 
 
 def create_gl_entry(doc_type, account, dr, cr, party=None):
@@ -11,11 +12,35 @@ def create_gl_entry(doc_type, account, dr, cr, party=None):
         'voucher_no': doc_type.name,
         'posting_date': doc_type.posting_date,
         'account': account,
-        'debit_amount': dr,
-        'credit_amount': cr,
+        'debit': dr,
+        'credit': cr,
         'party': party,
     }
-    print(options)
     gl_entry = frappe.get_doc(options)
-    print(gl_entry)
     gl_entry.insert()
+
+
+def create_revere_gl_entry(voucher_type, voucher_no):
+    filters = {
+        'voucher_type': voucher_type,
+        'voucher_no': voucher_no,
+    }
+    print(filters)
+    gl_entries = frappe.get_all('GL Entry', filters=filters, fields=['*'])
+
+    frappe.db.sql("""UPDATE `tabGL Entry` SET is_cancelled = 1,
+        modified=%s, modified_by=%s
+        where voucher_type=%s and voucher_no=%s and is_cancelled = 0""",
+                  (now(), frappe.session.user, gl_entries[0].voucher_type, gl_entries[0].voucher_no))
+
+    for gl_entry in gl_entries:
+        gl_entry.debit_amount, gl_entry.credit_amount = gl_entry.credit_amount, gl_entry.debit_amount
+        gl_entry.remarks = 'Cancelled'
+        gl_entry.is_cancelled = 1
+
+        print('------------------')
+        new_gl_entry = frappe.new_doc('GL Entry')
+        new_gl_entry.update(gl_entry)
+        new_gl_entry.insert()
+        new_gl_entry.submit()
+        print('===================')
--- accounting/accounting/doctype/sales_invoice/sales_invoice.py
+++ accounting/accounting/doctype/sales_invoice/sales_invoice.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2021, ac and contributors
 # For license information, please see license.txt
 
-from accounting.accounting.doctype.gl_entry.utils import create_gl_entry
+from accounting.accounting.doctype.gl_entry.utils import create_gl_entry, create_revere_gl_entry
 from frappe.model.document import Document
 
 
@@ -9,3 +9,6 @@ class SalesInvoice(Document):
 	def on_submit(self):
 		create_gl_entry(self, self.debit_to, self.total_amount, 0)
 		create_gl_entry(self, self.income_account, 0, self.total_amount)
+
+	def on_cancel(self):
+		create_revere_gl_entry(self.doctype, self.name)
