81ab181a0d05fae602aafa9d073a326c0f699264
ChillarAnand <anand21nanda@gmail.com>
2021-05-25 16:58:56 +0530

    feat: Added pnl for balance sheet

--- accounting/accounting/report/balance_sheet/balance_sheet.py
+++ accounting/accounting/report/balance_sheet/balance_sheet.py
@@ -8,126 +8,137 @@ from frappe import _
 
 
 def execute(filters=None):
-	from_date = filters.get('from_date')
-	to_date = filters.get('to_date')
-	date_range = [from_date, to_date]
+    from_date = filters.get('from_date')
+    to_date = filters.get('to_date')
+    date_range = [from_date, to_date]
 
-	columns = [
-		{
-			'fieldname': 'account',
-			'label': _('Account'),
-			'fieldtype': 'Link',
-			'options': 'Account',
-			'width': '200px',
-		},
-		{
-			'fieldname': 'balance',
-			'label': _('Balance'),
-			'fieldtype': 'Currency',
-			'width': '100px',
-		}
-	]
+    columns = [
+        {
+            'fieldname': 'account',
+            'label': _('Account'),
+            'fieldtype': 'Link',
+            'options': 'Account',
+            'width': '200px',
+        },
+        {
+            'fieldname': 'balance',
+            'label': _('Balance'),
+            'fieldtype': 'Currency',
+            'width': '100px',
+        }
+    ]
 
-	accounts = []
-	accounts.extend(get_accounts(account_type='Asset'))
-	accounts.extend(get_accounts(account_type='Liability'))
+    accounts = []
+    accounts.extend(get_accounts(account_type='Asset'))
+    accounts.extend(get_accounts(account_type='Liability'))
 
-	for account in accounts:
-		account['account'] = account['name']
+    for account in accounts:
+        account['account'] = account['name']
 
-	accounts = indent_accounts(accounts)
+    accounts = indent_accounts(accounts)
 
-	accounts = get_balances(accounts, date_range)
-	accounts = propagate_balances(accounts)
+    accounts = get_balances(accounts, date_range)
+    accounts = propagate_balances(accounts)
 
-	data = accounts
-	# pprint(accounts)
-	# print('=========')
+    accounts.append(get_profit_and_loss(accounts))
+    data = accounts
 
-	return columns, data
+    return columns, data
+
+
+def get_profit_and_loss(accounts):
+    asset = next((account for account in accounts if account['name'] == 'Assets'))
+    liability = next((account for account in accounts if account['name'] == 'Liabilities'))
+    balance = asset['balance'] - liability['balance']
+
+    pnl = {
+        'account': 'Provisional Profit/Loss',
+        'balance': balance,
+    }
+    return pnl
 
 
 def get_children(accounts, account):
-	children = []
-	for _account in accounts:
-		if _account['parent_account'] == account['name']:
-			children.append(_account)
-	return children
+    children = []
+    for _account in accounts:
+        if _account['parent_account'] == account['name']:
+            children.append(_account)
+    return children
 
 
 def propagate_balances(accounts):
-	for account in accounts:
-		if account['balance'] is None:
-			account['balance'] = get_cumulative_balance(accounts, account)
+    for account in accounts:
+        if account['balance'] is None:
+            account['balance'] = get_cumulative_balance(accounts, account)
 
-	return accounts
+    return accounts
 
 
 def get_accounts(account_type=None):
-	filters = {}
+    filters = {}
 
-	if account_type:
-		filters = {'account_type': ['=', account_type]}
-	fields = ['name', 'parent_account', 'lft', 'account_type', 'account_type', 'is_group']
+    if account_type:
+        filters = {'account_type': ['=', account_type]}
+    fields = ['name', 'parent_account', 'lft', 'account_type', 'account_type', 'is_group']
 
-	accounts = frappe.get_all('Account', fields=fields, filters=filters, order_by='lft')
-	return accounts
+    accounts = frappe.get_all('Account', fields=fields, filters=filters, order_by='lft')
+    return accounts
 
 
 def get_account_balance(account, date_range):
-	query = f'''
-	SELECT
-		SUM(debit) - SUM(credit)
-	FROM
-		`tabGL Entry`
-	WHERE
-		account="{account}" and is_cancelled=0 and
-		posting_date >= "{date_range[0]}" and posting_date <= "{date_range[1]}"
-	'''
-	result = frappe.db.sql(query)
-	return result[0][0]
+    query = f'''
+    SELECT
+        SUM(debit) - SUM(credit)
+    FROM
+        `tabGL Entry`
+    WHERE
+        account="{account}" and is_cancelled=0 and
+        posting_date >= "{date_range[0]}" and posting_date <= "{date_range[1]}"
+    '''
+    result = frappe.db.sql(query)
+    return result[0][0]
 
 
 def get_cumulative_balance(accounts, account):
-	children = get_children(accounts, account)
+    children = get_children(accounts, account)
 
-	if not children:
-		return account['balance']
+    if not children:
+        return account['balance']
 
-	for child in children:
-		if child['balance'] is None:
-			child['balance'] = get_cumulative_balance(accounts, child)
+    for child in children:
+        if child['balance'] is None:
+            child['balance'] = get_cumulative_balance(accounts, child)
 
-	balance = 0
-	for child in children:
-		balance += child['balance']
+    balance = 0
+    for child in children:
+        balance += child['balance']
 
-	return balance
+    return balance
+
 
 def get_balances(accounts, date_range):
-	for account in accounts:
-		balance = get_account_balance(account['name'], date_range)
-		if balance and balance < 0:
-			balance = abs(balance)
-		account['balance'] = balance
+    for account in accounts:
+        balance = get_account_balance(account['name'], date_range)
+        if balance and balance < 0:
+            balance = abs(balance)
+        account['balance'] = balance
 
-	return accounts
+    return accounts
 
 
 def indent_accounts(accounts):
-	for account in accounts:
-		depth = 0
-		current_account = account
-		parent = current_account['parent_account']
-		while parent is not None:
+    for account in accounts:
+        depth = 0
+        current_account = account
+        parent = current_account['parent_account']
+        while parent is not None:
+            depth += 1
+            current_account = frappe.get_list(
+                'Account', fields=['name', 'parent_account'],
+                filters={'name': parent}
+            )
+            parent = current_account[0]['parent_account']
 
-			depth += 1
-			current_account = frappe.get_list(
-				'Account', fields=['name', 'parent_account'],
-				filters={'name': parent}
-			)
-			parent = current_account[0]['parent_account']
+        account.indent = depth
 
-		account.indent = depth
-
-	return accounts
+    return accounts
