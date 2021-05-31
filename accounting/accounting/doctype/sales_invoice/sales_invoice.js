8f2ff9e7654a211a608655ebf433dca5cff1b4df
ChillarAnand <anand21nanda@gmail.com>
2021-05-31 18:41:15 +0530

    feat: Add custom buttons for sales invoice

--- accounting/accounting/doctype/sales_invoice/sales_invoice.js
+++ accounting/accounting/doctype/sales_invoice/sales_invoice.js
@@ -2,8 +2,8 @@
 // For license information, please see license.txt
 
 
-frappe.ui.form.on("Sales Invoice", "onload", function(frm) {
-    frm.set_query("party", function() {
+frappe.ui.form.on("Sales Invoice", "onload", function (frm) {
+    frm.set_query("party", function () {
         return {
             "filters": {
                 "party_type": "Customer"
@@ -11,7 +11,7 @@ frappe.ui.form.on("Sales Invoice", "onload", function(frm) {
         };
     });
 
-    frm.set_query("debit_to", function() {
+    frm.set_query("debit_to", function () {
         return {
             "filters": {
                 "root_type": "Accounts Receivable",
@@ -20,7 +20,7 @@ frappe.ui.form.on("Sales Invoice", "onload", function(frm) {
         };
     });
 
-    frm.set_query("income_account", function() {
+    frm.set_query("income_account", function () {
         return {
             "filters": {
                 "root_type": "Income",
@@ -29,62 +29,81 @@ frappe.ui.form.on("Sales Invoice", "onload", function(frm) {
         };
     });
 
+    frm.toggle_display("opening_balance", frm.doc.is_group == 1);
+
+    if (frm.doc.docstatus == 1) {
+        frm.add_custom_button(__('Create Payment Entry'), function () {
+            frappe.route_options = {
+                'party': frm.doc.party,
+                'amount': frm.doc.total_amount,
+                'account_paid_from': frm.doc.debit_to,
+            };
+            frappe.set_route('Form', 'Payment Entry', 'new-payment-entry');
+        });
+
+        frm.add_custom_button(__('General Ledger Report'), function () {
+            frappe.route_options = {
+                'voucher_no': frm.doc.name,
+            };
+            frappe.set_route('query-report', 'General Ledger');
+        });
+
+    }
+});
+
+frappe.ui.form.on('Sales Invoice', 'is_group', function (frm, cdt, cdn) {
+    console.log('aaa');
+    frm.toggle_display("opening_balance", frm.doc.is_group == 1);
+});
+
+frappe.ui.form.on("refresh", function (frm) {
+    console.log('aaa');
+    frm.toggle_display("opening_balance", frm.doc.is_group == 1);
 });
 
 
-//frappe.ui.form.on("Sales Invoice Item", {
-//	rate: function(frm, cdt, cdn) {
-//		frm.set_query("party", function() {
-//			return {
-//				filters: [
-//					["Party", "party_type", "=", "Customer"]
-//				]
-//			}
-//		});
-//	}
-//});
-
-
-frappe.ui.form.on('Sales Invoice Item', 'item', function(frm, cdt, cdn) {
+frappe.ui.form.on('Sales Invoice Item', 'item', function (frm, cdt, cdn) {
     var item = locals[cdt][cdn];
     frappe.model.set_value(cdt, cdn, 'quantity', 1);
 });
 
 
-frappe.ui.form.on('Sales Invoice Item', 'quantity', function(frm, cdt, cdn) {
-    var item = locals[cdt][cdn];
-    var amount = item.quantity * item.rate;
-     frappe.model.set_value(cdt, cdn, 'amount', amount);
-});
-
-
-frappe.ui.form.on('Sales Invoice Item', 'rate', function(frm, cdt, cdn) {
+frappe.ui.form.on('Sales Invoice Item', 'quantity', function (frm, cdt, cdn) {
     var item = locals[cdt][cdn];
     var amount = item.quantity * item.rate;
     frappe.model.set_value(cdt, cdn, 'amount', amount);
 });
 
 
-frappe.ui.form.on('Sales Invoice Item', 'amount', function(frm, cdt, cdn) {
+frappe.ui.form.on('Sales Invoice Item', 'rate', function (frm, cdt, cdn) {
+    var item = locals[cdt][cdn];
+    var amount = item.quantity * item.rate;
+    frappe.model.set_value(cdt, cdn, 'amount', amount);
+});
+
+
+frappe.ui.form.on('Sales Invoice Item', 'amount', function (frm, cdt, cdn) {
     var items = frm.doc.items;
     var total_amount = 0;
-    for(var i in items) {
+    for (var i in items) {
         total_amount = total_amount + items[i].amount;
-    };
+    }
+    ;
     frm.set_value('total_amount', total_amount);
 });
 
 
-frappe.ui.form.on('Sales Invoice Item', 'quantity', function(frm, cdt, cdn) {
+frappe.ui.form.on('Sales Invoice Item', 'quantity', function (frm, cdt, cdn) {
     var items = frm.doc.items;
     var total = 0;
-    for(var i in items) {
+    for (var i in items) {
         total = total + items[i].quantity;
-    };
+    }
+    ;
     frm.set_value('total_quantity', total);
 });
 
 
-frappe.form.link_formatters['Party'] = function(value, doc) {
+frappe.form.link_formatters['Party'] = function (value, doc) {
     return doc.party_name + ' (' + doc.party_type + ')';
 };
