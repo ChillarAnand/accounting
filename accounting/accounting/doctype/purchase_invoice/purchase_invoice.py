c5307652813bb014399c98a866016f61a03b91f0
ChillarAnand <anand21nanda@gmail.com>
2021-06-03 11:35:37 +0530

    chore: Add tests for purchase invoice

--- accounting/accounting/doctype/purchase_invoice/purchase_invoice.py
+++ accounting/accounting/doctype/purchase_invoice/purchase_invoice.py
@@ -5,10 +5,13 @@ import frappe
 from frappe.model.document import Document
 
 from accounting.accounting.doctype.gl_entry.utils import create_gl_entry
+from frappe.utils import flt
 
 
 class PurchaseInvoice(Document):
 	def validate(self):
+		self.pre_fill()
+		self.validate_quantity()
 		self.validate_dates()
 		
 	def validate_dates(self):
@@ -18,3 +21,15 @@ class PurchaseInvoice(Document):
 	def on_submit(self):
 		create_gl_entry(self, self.expense_account, self.total_amount, 0)
 		create_gl_entry(self, self.credit_to, 0, self.total_amount)
+
+	def pre_fill(self):
+		self.total_quantity, self.total_amount = 0.0, 0.0
+		for item in self.items:
+			item.update({'amount': flt(item.rate) * flt(item.quantity)})
+			self.total_quantity += flt(item.quantity)
+			self.total_amount += flt(item.amount)
+
+	def validate_quantity(self):
+		for item in self.items:
+			if item.quantity < 1:
+				frappe.throw('Item quantity should not be less than 1')
--- accounting/accounting/doctype/purchase_invoice/test_purchase_invoice.py
+++ accounting/accounting/doctype/purchase_invoice/test_purchase_invoice.py
@@ -1,8 +1,36 @@
 # Copyright (c) 2021, ac and Contributors
 # See license.txt
-
-# import frappe
 import unittest
 
+import frappe
+from frappe import ValidationError
+
+
 class TestPurchaseInvoice(unittest.TestCase):
-	pass
+	def test_purchase_invoice_validation_errors(self):
+		with self.assertRaises(ValidationError):
+			self.create_purchase_invoice('Frappe', 'Laptop', 0)
+			self.create_purchase_invoice('Frappe', 'Laptop', -1)
+	
+	def test_create_purchase_invoice(self):
+		invoice = self.create_purchase_invoice('Frappe', 'Laptop', 2)
+		invoice.submit()
+		
+		total = sum(item.quantity * item.rate for item in invoice.items)
+		assert invoice.total_amount == total
+	
+	@staticmethod
+	def create_purchase_invoice(party, item_name, quantity):
+		invoice = frappe.new_doc("Purchase Invoice")
+		invoice.party = party
+		invoice.posting_date = frappe.utils.nowdate()
+		invoice.set("items", [
+			{
+				"item": item_name,
+				"quantity": quantity,
+				"rate": 11200,
+			}
+		])
+		invoice.insert()
+		
+		return invoice
