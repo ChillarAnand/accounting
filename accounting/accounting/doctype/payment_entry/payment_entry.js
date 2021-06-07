012f7eda79a876f53e06bf2a24bdd34dac23376d
ChillarAnand <anand21nanda@gmail.com>
2021-06-07 15:02:20 +0530

    fix: Fix issues with accounts on payment entry

--- accounting/accounting/doctype/payment_entry/payment_entry.js
+++ accounting/accounting/doctype/payment_entry/payment_entry.js
@@ -2,12 +2,56 @@
 // For license information, please see license.txt
 
 
-frappe.ui.form.on('Payment Entry', 'onload', function(frm) {
-    frm.set_query('party', function(){
+frappe.ui.form.on('Payment Entry', 'onload', function (frm) {
+    frm.set_query('party', function () {
         return {
             'filters': {
                 'party_type': frm.doc.party_type
             }
         }
     })
+
+    frm.set_query('party', function () {
+        return {
+            'filters': {
+                'party_type': frm.doc.party_type
+            }
+        }
+    })
+
+    frm.set_query("account_paid_from", function () {
+        if (frm.doc.payment_type == 'Pay') {
+            return {
+                'filters': {
+                    'is_group': 0,
+                    'root_type': 'Asset',
+                }
+            };
+        } else {
+            return {
+                'filters': {
+                    'is_group': 0,
+                }
+            };
+        }
+    });
+
+    frm.set_query("account_paid_to", function () {
+        if (frm.doc.payment_type == 'Pay') {
+            return {
+                'filters': {
+                    'is_group': 0,
+                }
+            };
+        } else {
+            return {
+                'filters': {
+                    'is_group': 0,
+                    'root_type': 'Asset',
+
+                }
+            };
+        }
+    });
+
 })
