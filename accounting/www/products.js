e4665df4dce863e0703b8b733255d200aa953c1c
ChillarAnand <anand21nanda@gmail.com>
2021-06-08 15:38:36 +0530

    feat: Added alert after adding item to cart

--- accounting/www/products.js
+++ accounting/www/products.js
@@ -3,13 +3,18 @@ frappe.ready(function () {
             if (frappe.session.user === 'Guest') {
                 window.location.href = "/login"
             } else {
+                var itemName = $(e.currentTarget).data('item-name');
                 frappe.call({
                     method: 'accounting.accounting.doctype.sales_invoice.sales_invoice.add_to_cart',
                     args: {
                         'username': frappe.session.user,
-                        'item_name': $(e.currentTarget).data('item-name'),
+                        'item_name': itemName,
                     }
-                })
+                });
+                frappe.show_alert({
+                    message:__(itemName + ' added to cart'),
+                    indicator:'green'
+                }, 4);
             }
         })
     }
